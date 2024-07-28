1e3be1de53393c17dafecada549f6ada4c8723ce
libalpm <159192189+libalpm64@users.noreply.github.com>
2024-07-28 04:10:10 -0400

    Create system_meta.js

---
+++ tampermonkey script/system_meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Smart Chess Bot: The Ultimate Chess Analysis System
+// @namespace   Libalpm
+// @author      Libalpm
+// @version     8.6
+// @homepageURL https://github.com/sayfpack13/chess-analysis-bot
+// @supportURL  https://mmgc.ninja/
+// @match       https://www.chess.com/*
+// @match       https://lichess.org/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_xmlhttpRequest
+// @grant       GM_getResourceText
+// @grant       GM_registerMenuCommand
+// @description 	Our chess analysis system is designed to give players the edge they need to win. By using advanced algorithms and cutting-edge technology, our system can analyze any chess position and suggest the best possible move, helping players to make smarter and more informed decisions on the board.
+// @description:fr 	Notre système d'analyse d'échecs est conçu pour donner aux joueurs l'avantage dont ils ont besoin pour gagner. En utilisant des algorithmes avancés et des technologies de pointe, notre système peut analyser n'importe quelle position d'échecs et suggérer le meilleur coup possible, aidant les joueurs à prendre des décisions plus intelligentes et plus éclairées sur l'échiquier.
+// @require     https://greasyfork.org/scripts/460400-usergui-js/code/userguijs.js?version=1157130
+// @run-at      document-start
+// @inject-into content
+// ==/UserScript==
